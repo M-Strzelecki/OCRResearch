3764cab16da10e0d13204d2677b8ae11d16a783b
Miko Windows <michael.strzelecki23@gmail.com>
2023-05-19 16:18:52 +0100

    nothing

--- compareSMAPE.py
+++ compareSMAPE.py
@@ -7,10 +7,10 @@ Compare two csv files and return a string representation of the difference betwe
 hard = "./hardcodednutrilabels/hardfulltext.csv"
 sample = "./fulltextfrompipeline/fulltext_v1_4.csv"
 
-result = prep.count_chars_in_file("./hardcodednutrilabels/hardfulltext.csv")
-# prep.print_individual_count(result)
+# result = prep.count_chars_in_file("./hardcodednutrilabels/hardfulltext.csv")
+# # prep.print_individual_count(result)
 
-result2 = prep.count_chars_in_file("./fulltextfrompipeline/fulltext.csv")
+# result2 = prep.count_chars_in_file("./fulltextfrompipeline/fulltext.csv")
 # prep.print_individual_count(result2)
 
 prep.compare_individual_csv_files(hard, sample)
